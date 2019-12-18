7fe8c3fdef40141c834bfed1d8020fd336e13bb1
Ronan C <rojocas@gmail.com>
2019-12-18 16:06:14 +0000

    Started collapsable Navbar media qeuries

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // Scroll Animation
 
-$('.navbar a').on('click', function (e) {
+$('.navbar a, .home-title a').on('click', function (e) {
     if (this.hash !== '') {
         e.preventDefault();
 
@@ -10,3 +10,19 @@ $('.navbar a').on('click', function (e) {
         }, 800);
     }
 })
+
+
+
+// Collapse Navbar
+
+$(function() {
+    $(".toggle").on("click", function() {
+        if ($(".menu-item").hasClass("active")) {
+            $(".menu-item").removeClass("active");
+            $(this).find("a").html("<i class='fas fa-bars'></i>");
+        } else {
+            $(".menu-item").addClass("active");
+            $(this).find("a").html("<i class='fas fa-times'></i>");
+        }
+    });
+});
